89dacb653b5808353f9bb73b96786eca0a050689
prateekbanra7 <66116227+prateekbanra7@users.noreply.github.com>
2025-04-02 19:30:38 +0530

    Add files via upload

---
+++ SelectApp.java
@@ -0,0 +1,85 @@
+package in.abc.main;
+
+//rt.jar => jdk s/w
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+//MYSQL-JAR => given by mysql db vendor
+import com.mysql.cj.jdbc.Driver;
+
+public class SelectApp {
+
+	public static void main(String[] args) throws SQLException {
+
+		// 1.Loading and Register the Driver
+//		Driver driver = new Driver();
+//		DriverManager.registerDriver(driver);
+
+		//resources used in jdbc application
+		Connection connection = null;
+		Statement statement = null;
+		ResultSet resultSet = null;
+
+		// 2. Establish the Connection
+		String url = "jdbc:mysql://localhost:3306/enterprisejavabatch1";
+		String user = "root";
+		String password = "root";
+
+		try {
+
+			connection = DriverManager.getConnection(url, user, password);
+
+			if (connection != null) {
+
+				// 3. Creation of Statement Object to move to the location using connection)
+				statement = connection.createStatement();
+
+				if (statement != null) {
+
+					String sqlSelectQuery = "select sid,sname,sage,saddr from student";
+
+					// 4.Using Statement Object execute the Query
+					resultSet = statement.executeQuery(sqlSelectQuery);
+
+					if (resultSet != null) {
+
+						// 5. Process the resultSet to get the data
+						System.out.println("SID\tSNAME\tSAGE\tSADDR");
+						System.out.println("=============================");
+						while (resultSet.next()) {
+							int sid = resultSet.getInt("sid");
+							String sname = resultSet.getString("sname");
+							int sage = resultSet.getInt("sage");
+							String saddr = resultSet.getString("saddr");
+							System.out.println(sid + "\t" + sname + "\t" + sage + "\t" + saddr);
+						}
+					}
+				}
+			}
+		} catch (SQLException se) {
+			se.printStackTrace();
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			// 6. closing the resources used
+
+			if (resultSet != null) {
+				resultSet.close();
+
+			}
+			if (statement != null) {
+				statement.close();
+
+			}
+			if (connection != null) {
+				connection.close();
+
+			}
+		}
+
+	}
+
+}
---
+++ UpdateApp.java
@@ -0,0 +1,74 @@
+package in.abc.main;
+
+//rt.jar => jdk s/w
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+//MYSQL-JAR => given by mysql db vendor
+import com.mysql.cj.jdbc.Driver;
+
+public class UpdateApp {
+
+	public static void main(String[] args) throws SQLException {
+
+		// 1.Loading and Register the Driver
+//		Driver driver = new Driver();
+//		DriverManager.registerDriver(driver);
+
+		// resources used in jdbc application
+		Connection connection = null;
+		Statement statement = null;
+		ResultSet resultSet = null;
+
+		// 2. Establish the Connection
+		String url = "jdbc:mysql://localhost:3306/enterprisejavabatch1";
+		String user = "root";
+		String password = "root";
+
+		try {
+
+			connection = DriverManager.getConnection(url, user, password);
+
+			if (connection != null) {
+
+				// 3. Creation of Statement Object to move to the location using connection)
+				statement = connection.createStatement();
+
+				if (statement != null) {
+
+					String updateSqlQuery = "update student set sage = 30 where sid = 18";
+
+					// 4.Using Statement Object execute the Query
+					int noOfRows = statement.executeUpdate(updateSqlQuery);
+					System.out.println("No of rows updated is :: " + noOfRows);
+
+				}
+			}
+
+		} catch (SQLException se) {
+			se.printStackTrace();
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			// 6. closing the resources used
+
+			if (resultSet != null) {
+				resultSet.close();
+
+			}
+			if (statement != null) {
+				statement.close();
+
+			}
+			if (connection != null) {
+				connection.close();
+
+			}
+		}
+
+	}
+
+}
